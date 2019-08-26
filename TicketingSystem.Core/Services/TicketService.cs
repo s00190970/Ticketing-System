ab84c10228be23b14641d2cfa56cc7974c1a3b22
Cristian <beckertcrysty@yahoo.com>
2019-08-26 23:00:21 +0300

    automatic created datetime

--- TicketingSystem.Core/Services/TicketService.cs
+++ TicketingSystem.Core/Services/TicketService.cs
@@ -41,6 +41,7 @@ namespace TicketingSystem.Core.Services
 
         public TicketDto Add(TicketDto item)
         {
+            item.OpenDateTime = DateTime.Now;
             Ticket ticket = _converter.DtoToModel(item);
             TicketDto ticketDto = _converter.ModelToDto(_repository.Add(ticket));
             return ticketDto;
